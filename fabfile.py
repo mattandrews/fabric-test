1998c3ec7a44ff6786e3ea381cd65063d2fe59a6
Matt Andrews <matt@mattandrews.info>
2014-05-01 23:46:00 +0100

    test 2

---
+++ fabfile.py
@@ -0,0 +1,22 @@
+from __future__ import with_statement
+from fabric.api import *
+from fabric.contrib.console import confirm
+
+
+env.hosts = ['matt@162.243.234.162:25000']
+
+def commit():
+    local("git add . && git commit")
+
+def push():
+    local("git push")
+
+def prepare_deploy():
+    commit()
+    push()
+
+def deploy():
+    prepare_deploy()
+    code_dir = '/var/www/scenepointblank/assets/fabric-test'
+    with cd(code_dir):
+        run("git pull")
